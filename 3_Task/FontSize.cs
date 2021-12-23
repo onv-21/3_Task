0ccf39febe1df43ba3e18500c13173cb98185b93
Администратор <@DESKTOP-PJPICF5>
2021-12-23 18:17:48 +0800

    Добавьте файлы проекта.

---
+++ 3_Task/FontSize.cs
@@ -0,0 +1,12 @@
+﻿namespace _3_Task
+{
+    internal class FontSize
+    {
+        private double fontSize;
+
+        public FontSize(double fontSize)
+        {
+            this.fontSize = fontSize;
+        }
+    }
+}
